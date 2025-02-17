cdff567497bba3faf48dc4dea4e37bc1fbb8e49a
porarisuu <112392424+PolaDasma123@users.noreply.github.com>
2025-02-18 02:21:35 +0800

    Update page.jsx

--- app/dashboard/products/[id]/page.jsx
+++ app/dashboard/products/[id]/page.jsx
@@ -1,71 +1,37 @@
-import Image from "next/image"
-import Link from "next/link"
-import styles from "@/app/ui/dashboard/products/products.module.css";
-import Search from "@/app/ui/dashboard/search/search";
-import Pagination from "@/app/ui/dashboard/pagination/pagination";
-import {searchParams} from "next/navigation"
-import { fetchReports, fetchUser } from "@/app/lib/data";
+import styles from "@/app/ui/dashboard/products/singleProduct/singleProduct.module.css";
+import Image from "next/image";
+import { fetchReport } from "@/app/lib/data";
 
 
-const Productspage = async ({searchParams}) => {
-    const q = searchParams?.q || "";
-    const page = searchParams?.page || 1 ;
-    const { count, reports } = await fetchReports(q, page);
+const SingleProductPage = async ({ params }) => {
 
-    // Fetch user names for each report
-    const userPromises = reports.map(report => fetchUser(report.userId));
-    const users = await Promise.all(userPromises);
-
+     const { id } = params;
+    const report = await fetchReport(id)
     return (
         <div className={styles.container}>
-        <div className={styles.top}>
-            <Search placeholder="Search Product..." />
-            <Link href="/dashboard/products/add">
-            <button className={styles.addButton} disabled>Add New</button>
-            </Link>
+            <div className={styles.formContainer}>
+                <form action="" className={styles.form}>
+                <input type="hidden" name="id" value={report.id} />
+                    <label>User ID</label>
+                    <input type="text" name="userId" placeholder={report.userId}/>
+                    <label>reportedItemId</label>
+                    <input type="number" name="price" placeholder={report.reportedItemId}/>
+                    <label>Report count</label>
+                    <input type="number" name="stock" placeholder={report.reportCount}/>
+                    <label>Reason</label>
+                    <input type="text" name="reason" placeholder={report.reason}/>
+                    <label>Type</label>
+                    <input type="text" name="itemType" placeholder={report.itemType}/>
+                    <label>Report Date</label>
+                    <textarea type="text" name="size" placeholder={report.reportDate?.toString().slice(4, 16)}/>                   
+                    <label>Report Title</label>
+                    <input name="desc" id="desc" rows="10" placeholder={report.reportTitle}/>
+
+                    <button>Update</button>
+                </form>
+            </div>
         </div>
-        <table className={styles.table}>
-            <thead>
-                <tr>
-                    <td>Reported At</td>
-                    <td>Report Count</td>
-                    <td>Name</td> 
-                    <td>Reason</td>
-                    <td>Action</td>
-                </tr>
-            </thead>
-            <tbody>
-                {reports.map((report, index) => (
-                <tr key={report.id}>
-                    <td>
-                        <div className={styles.product}>
-                            
-                        {report.reportDate?.toString().slice(4, 16)}
-                        </div>
-                    </td>
-                    <td>{report.reportCount}</td>
-                    <td>{users[index]?.name}</td>
-                    <td>{report.reportTitle}</td>
-                    
-                    <td>
-                        <div className={styles.buttons}>
-                            <Link href={`/dashboard/products/${report.id}`}>
-                                <button className={`${styles.button} ${styles.view}`}>
-                                    View
-                                </button>
-                            </Link>
-                                <button className={`${styles.button} ${styles.delete}`}>
-                                    Delete
-                                </button>
-                        </div>
-                    </td>
-                </tr>
-                ))}
-            </tbody>
-        </table>
-        <Pagination count={count} />
-    </div>
     )
 }
 
-export default Productspage
+export default SingleProductPage
