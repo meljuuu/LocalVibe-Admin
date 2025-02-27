acf955c7b890ed4b30a5939d54d50dbb086d6793
Venjami <jaminalraven@gmail.com>
2025-02-27 14:27:14 +0800

    fix

--- app/dashboard/users/[id]/page.jsx
+++ app/dashboard/users/[id]/page.jsx
@@ -6,15 +6,15 @@ import { decryptData } from "@/app/utils/encryption";
 
 const SingleUserPage = async ({ params }) => {
     const { id } = params;
-    const user = await fetchUser(id)
+    const user = await fetchUser(id);
 
     const decryptedEmail = decryptData(user.email);
 
     return (
         <div className={styles.container}>
             <div className={styles.infoContainer}>
-                <div className={styles.imgContainer}></div>
-                    <Image src={user.img || "/noavatar.png" } alt="" fill/>
+                <div className={styles.imgContainer}>
+                    <Image src={user.img || "/noavatar.png"} alt="" fill />
                 </div>
                 {user.name}
             </div>
@@ -22,21 +22,20 @@ const SingleUserPage = async ({ params }) => {
                 <form action={updateUser} className={styles.form}>
                     <input type="hidden" name="id" value={user.id} />
                     <label>Username</label>
-                    <input type="text" name="username" placeholder={user.userName}/>
+                    <input type="text" name="username" placeholder={user.userName} />
                     <label>Email</label>
-                    <input type="email" name="email" placeholder={decryptedEmail}/>
+                    <input type="email" name="email" placeholder={decryptedEmail} />
                     <label>Password</label>
-                    <input type="Password" name="password"/>
+                    <input type="password" name="password" />
                     <label>Name</label>
-                    <input type="text" name="name" placeholder={user.name}/>
+                    <input type="text" name="name" placeholder={user.name} />
                     <label>Account type</label>
-                    <textarea type="text" name="acctype" placeholder={user.accountType}/>
-
+                    <textarea type="text" name="acctype" placeholder={user.accountType} />
                     <button>Update</button>
                 </form>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default SingleUserPage
+export default SingleUserPage;
