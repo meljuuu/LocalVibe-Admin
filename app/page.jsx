65a6ebff985b0dc8e8df412b3e95b78f87f9586b
VenJami <polarisjumel@gmail.com>
2025-05-07 00:08:18 +0800

    password

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,9 @@
 
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import Image from 'next/image';
-import eyeOpen from './eyeOpen.png';
-import eyeClose from './eyeClose.png';
+import Image from "next/image";
+import eyeOpen from "./eyeOpen.png";
+import eyeClose from "./eyeClose.png";
 
 const Homepage = () => {
   const [username, setUsername] = useState("");
@@ -14,7 +14,10 @@ const Homepage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (username === "admin" && password === "LocalAdmin2025!") {
+    const adminUsername = process.env.NEXT_PUBLIC_ADMIN_USERNAME;
+    const adminPassword = process.env.NEXT_PUBLIC_ADMIN_PASSWORD;
+
+    if (username === adminUsername && password === adminPassword) {
       router.push("/dashboard");
     } else {
       alert("Invalid username or password");
@@ -27,7 +30,9 @@ const Homepage = () => {
         <h2 className="text-2xl font-bold text-center mb-6">Admin Login</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
-            <label className="block text-sm font-medium text-gray-700">Username</label>
+            <label className="block text-sm font-medium text-gray-700">
+              Username
+            </label>
             <input
               type="text"
               className="mt-1 block w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -35,8 +40,10 @@ const Homepage = () => {
               onChange={(e) => setUsername(e.target.value)}
             />
           </div>
-          <div style={{position: 'relative'}}>
-            <label className="block text-sm font-medium text-gray-700">Password</label>
+          <div style={{ position: "relative" }}>
+            <label className="block text-sm font-medium text-gray-700">
+              Password
+            </label>
             <div className="relative flex flex-row">
               <input
                 type={showPassword ? "text" : "password"}
@@ -48,12 +55,20 @@ const Homepage = () => {
                 type="button"
                 onClick={() => setShowPassword(!showPassword)}
                 className=" p-0 border-0 items-center"
-                style={{ backgroundColor: 'transparent', padding: 0, border: 0, position: 'absolute', right: 5, alignItems: 'center', transform: 'translateY(50%)' }}
+                style={{
+                  backgroundColor: "transparent",
+                  padding: 0,
+                  border: 0,
+                  position: "absolute",
+                  right: 5,
+                  alignItems: "center",
+                  transform: "translateY(50%)",
+                }}
               >
                 <Image
                   src={showPassword ? eyeClose : eyeOpen}
                   alt="toggle password visibility"
-                  width={'100%'}
+                  width={"100%"}
                   height={20}
                 />
               </button>
