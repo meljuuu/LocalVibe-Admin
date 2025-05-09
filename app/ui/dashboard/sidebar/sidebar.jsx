cf696f8bfb819b1856f8f0274c37307807bdccca
VenJami <polarisjumel@gmail.com>
2025-05-09 12:48:19 +0800

    123

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,65 +1,67 @@
 import Image from "next/image";
 import Menulink from "./menuLink/menuLink";
-import styles from "./sidebar.module.css"
+import styles from "./sidebar.module.css";
 import {
-    MdDashboard,
-    MdSupervisedUserCircle,
-    MdAnalytics,
-    MdLogout,
-  } from "react-icons/md";
+  MdDashboard,
+  MdSupervisedUserCircle,
+  MdAnalytics,
+  MdLogout,
+} from "react-icons/md";
 const menuItems = [
-    {
-      title: "Pages",
-      list: [
-        {
-          title: "Dashboard",
-          path: "/dashboard",
-          icon: <MdDashboard />,
-        },
-        {
-          title: "Users",
-          path: "/dashboard/users",
-          icon: <MdSupervisedUserCircle />,
-        },
-        {
-          title: "Reported Content",
-          path: "/dashboard/products",
-          icon: <MdAnalytics />,
-        },
-        {
-          title: "Admin",
-          path: "/dashboard/admin",
-          icon: <MdSupervisedUserCircle />,
-        },
-      ],
-    },
-  ];
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Users",
+        path: "/dashboard/users",
+        icon: <MdSupervisedUserCircle />,
+      },
+      {
+        title: "Reported Content",
+        path: "/dashboard/products",
+        icon: <MdAnalytics />,
+      },
+    ],
+  },
+];
 
 const Sidebar = () => {
-    return (
-        <div className={styles.container}>
-          <div className={styles.user}>
-            <Image className={styles.userImage} src="/noavatar.png" alt="" width="50" height="50"/>
-            <div className={styles.userDetail}>
-              <span className={styles.username}>Harvey Samson</span>
-              <span className={styles.userTitle}>Administrator</span>
-            </div>
-          </div>
-            <ul className={styles.list}>
-            {menuItems.map(cat=>(
-                <li key={cat.title}>
-                  <span className={styles.cat}>{cat.title}</span>
-                  {cat.list.map(item=>(
-                    <Menulink item={item} key={item.title} />
-                  ))}
-                </li>
-                ))}
-            </ul>
-            <button className={styles.logout}>
-            <MdLogout />
-              Logout</button>
+  return (
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <Image
+          className={styles.userImage}
+          src="/noavatar.png"
+          alt=""
+          width="50"
+          height="50"
+        />
+        <div className={styles.userDetail}>
+          <span className={styles.username}>Harvey Samson</span>
+          <span className={styles.userTitle}>Administrator</span>
         </div>
-    );
+      </div>
+      <ul className={styles.list}>
+        {menuItems.map((cat) => (
+          <li key={cat.title}>
+            <span className={styles.cat}>{cat.title}</span>
+            {cat.list.map((item) => (
+              <Menulink item={item} key={item.title} />
+            ))}
+          </li>
+        ))}
+      </ul>
+      <button className={styles.logout}>
+        <MdLogout />
+        Logout
+      </button>
+    </div>
+  );
 };
 
-export default Sidebar
+export default Sidebar;
